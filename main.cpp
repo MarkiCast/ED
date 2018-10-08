5363f55a7b7023e97c35b1fda6f82ec959917886
gabwesz <37197353+gabwesz@users.noreply.github.com>
2018-10-08 16:58:36 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,56 +9,75 @@
 
 using namespace std;
 
+string readfilename() {
+  cout << "Digite o nome do arquivo xml a ser lido: " << end1;
+  string xmlfile;
+  cin >> xmlfile;
+  return xmlfile;
+}
+
+void parser();
+
 int main() {
 
-    string xmlfilename;
-    //std::cin >> xmlfilename;  // entrada
+    string xmlfilename = readfilename();
 
-    ifstream myfile ("dataset01.xml");
+    //Abre o xml
+    ifstream myfile;
+    myfile.open(xmlfilename);
 
+    //Cria a pilha
     auto pile = structures::LinkedStack<string> {};
-    size_t i;
-    size_t e;
-    string tag;
-    string line;
-    size_t pos = 0;
-    int lenght = 0;
+
+    //Parser deve ser chamado aqui(como fazer?)
 
     cout << "funciona" << "\n" << endl;
 
-    if(myfile.is_open()){
-      while(getline(myfile, line))  {
-        i = line.find_first_of('<', pos);
-        e = line.find_first_of('>', pos);
-        printf("continua funfando");
-        lenght = e-i;
-        tag =   line.substr(i,lenght);
-        printf("tag funciona");
-        if (tag.find('/')) {
-          tag = line.substr(i+1,lenght-1);
-          if (pile.empty()) {
-            printf("aki tbm da merda");
-          } else {
-            printf("ta convertendo pra char*");
-            auto first = tag.c_str();
-            auto second = pile.top().c_str();
-            if (strcmp(first, second) ==  0) {
-              cout << tag << "\n" << endl;
-              pile.pop();
-            } else {
-              printf("aki da merda");
-            }
-          }
-        } else {
-
-          pile.push(tag);
-        }
-        pos = e;
-      }
-      if (pile.empty()) {
-        printf("merda d novo");
-      }
-    }
 
   return 0;
 }
+
+void parser() {
+  size_t i;
+  size_t e;
+  string tag;
+  string line;
+  size_t pos = 0;
+  int lenght = 0;
+  if (!myfile.is_open()) {
+    throw invalid_argument("O arquivo não pode ser aberto.");
+  } else {
+    while(getline(myfile, line))  {
+      i = line.find_first_of('<', pos);
+      e = line.find_first_of('>', pos);
+      printf("continua funfando");
+      lenght = e-i;
+      tag =   line.substr(i,lenght);
+      printf("tag funciona");
+      if (tag.find('/')) {
+        tag = line.substr(i+1,lenght-1);
+        if (pile.empty()) {
+          printf("aki tbm da merda");
+        } else {
+          printf("ta convertendo pra char*");
+          auto first = tag.c_str();
+          auto second = pile.top().c_str();
+          if (strcmp(first, second) ==  0) {
+            cout << tag << "\n" << endl;
+            pile.pop();
+          } else {
+            throw invalid_argument("Topo da pilha incopatível.");
+          }
+        }
+      } else {
+        pile.push(tag);
+      }
+      pos = e;
+    }  //Fim do laço while
+    if (!pile.empty()) {
+      throw invalid_argument("Pilha não finaliza vazia.");
+    } else {
+      return;
+    }
+  } //Fim do else "arquivo aberto"
+}  //Fim da função parser()
